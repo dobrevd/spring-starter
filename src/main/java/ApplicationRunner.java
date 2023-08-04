96f74a8c31d245f3d946dbd127e204866f4af039
Dmitriy Dobrev <dobrev@ukr.net>
2023-08-04 17:45:28 +0300

    Spring. 17. Custom BeanPostProcessor. 2

--- src/main/java/ApplicationRunner.java
+++ src/main/java/ApplicationRunner.java
@@ -1,5 +1,6 @@
 import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CompanyRepository;
+import com.dmdev.spring.database.repository.CrudRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -9,8 +10,8 @@ public class ApplicationRunner {
             var pool1 = context.getBean("pool1", ConnectionPool.class);
             System.out.println(pool1);
 
-            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-            System.out.println(companyRepository);
+            var companyRepository = context.getBean("companyRepository", CrudRepository.class);
+            System.out.println(companyRepository.findById(1));
         }
     }
 }
