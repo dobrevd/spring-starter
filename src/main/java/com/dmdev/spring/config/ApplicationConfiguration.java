d36318539c6499baa901dcc2ff3e637d2408ceb3
Dmitriy Dobrev <dobrev@ukr.net>
2023-08-07 17:50:34 +0300

    Spring. 26. @Bean. 1

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,13 +1,16 @@
 package com.dmdev.spring.config;
 
+import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CrudRepository;
-import org.springframework.context.annotation.ComponentScan;
+import com.dmdev.spring.database.repository.UserRepository;
+import com.dmdev.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+@Import(WebConfiguration.class)//импорт класса из другого пакета
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.dmdev.spring",
@@ -18,4 +21,14 @@ import org.springframework.stereotype.Component;
         @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
     })
 public class ApplicationConfiguration {
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String userName,
+                                @Value("${db.pool.size}") Integer poolSize){
+        return new ConnectionPool(userName, poolSize);
+    }
+    @Bean
+    public UserRepository userRepository(ConnectionPool pool2){
+        return new UserRepository(pool2);
+    }
 }
